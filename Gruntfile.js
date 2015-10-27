a709125022277587dc28090cb39c096a2a1a5f18
insanegrv <insanegrv@gmail.com>
2015-10-27 20:38:00 +0300

    Add automation

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,24 +6,76 @@ module.exports = function(grunt) {
   var config = {
     pkg: grunt.file.readJSON('package.json'),
 
+    copy: {
+      build: {
+        files: [{
+          expand: true,
+          cwd: "src",
+          src: [
+            "img/**",
+            "js/**",
+            "index.html",
+            "form.html"
+          ],
+          dest: "build"
+        }]
+      }
+    }
+
+    clean: {
+      build: ["build"]
+    }
+
     sass: {
       style: {
         files: {
-          'css/style.css': 'sass/style.scss'
+          "build/css/style.css": ["src/sass/style.scss"]
         }
       }
     },
 
+    // cmq: {
+    //   style: {
+    //     files: {
+    //       "build/css/style.css" : ["build/css/style.css"]
+    //     }
+    //   }
+    // }
+
     postcss: {
       options: {
         processors: [
-          require('autoprefixer')({browsers: 'last 2 versions'})
+          require("autoprefixer")({browsers: "last 2 versions"})
         ]
       },
       style: {
-        src: 'css/*.css'
+        src: "build/css/*.css"
       }
     },
+
+    cssmin: {
+      options: {
+        keepSpecialComments: 0,
+        report: "gzip"
+      },
+      style: {
+        files: {
+          "build/css/style.min.css" : ["build/css/style.css"]
+        }
+      }
+    }
+
+    imagemin: {
+      images: {
+        options: {
+          optimizationLevel: 3
+        },
+        files: [{
+          expand: true,
+          src: ["build/img/**/*.{png, jpg, gif, svg}"]
+        }]
+      }
+    }
 
     watch: {
       style: {
@@ -36,6 +88,16 @@ module.exports = function(grunt) {
       }
     }
   };
+
+  grunt.registerTask("build", [
+    "clean",
+    "copy",
+    "sass",
+    // "cmq",
+    "postcss",
+    "cssmin",
+    "imagemin",
+    ]);
 
   config = require('./.gosha')(grunt, config);
 
